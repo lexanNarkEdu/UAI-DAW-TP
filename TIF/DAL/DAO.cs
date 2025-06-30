ae020a97c1a2e9c0b722c0aab96ae380bb85e451
condenl <nile.conde@gmail.com>
2025-06-29 22:59:21 -0300

    merge conflicts

--- TIF/DAL/DAO.cs
+++ TIF/DAL/DAO.cs
@@ -100,21 +100,6 @@ namespace DAL
             }
         }
 
-<<<<<<< HEAD
-        public void backup(string dbName, string backupPath)
-        {
-            string backupCommand = $@"
-                exec msdb.dbo.rds_backup_database 
-                    @source_db_name = '{dbName}',
-                    @s3_arn_to_backup_to = 'arn:aws:s3:::dawbackup/{backupPath}',
-                    @type = 'FULL';";
-            using (SqlCommand cmd = new SqlCommand(backupCommand, miConnection))
-            {
-                miConnection.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
-=======
         public int ExecuteNonQuery(string pCommandText, List<SqlParameter> parametros, CommandType tipo)
         {
             try
@@ -139,7 +124,5 @@ namespace DAL
                     miConnection.Close();
             }
         }
-
->>>>>>> main
     }
 }
